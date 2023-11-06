bf605c343e7ade1a2b905b09d4b6c41a030964d1
SunLxy <1011771396@qq.com>
2023-11-06 17:26:23 +0800

    feat:添加根据路径生成数据

--- core/src/utils/index.ts
+++ core/src/utils/index.ts
@@ -1,5 +1,7 @@
 
 import { CSTU_PathTypes } from "../CSTU_interface"
+import { CSTU_getValue } from "./CSTU_getValue"
+import { CSTU_setValue } from "./CSTU_setValue"
 
 export * from "./CSTU_getValue"
 export * from "./CSTU_setValue"
@@ -37,4 +39,14 @@ export const CSTU_splitPath = (path: string) => {
     return [path]
   }
   return []
+}
+
+/**获取路径数据*/
+export function CSTU_cloneByNamePathList(store: Record<string, any>, namePathList: CSTU_PathTypes[]): Record<string, any> {
+  let newStore = {};
+  namePathList.forEach(namePath => {
+    const value = CSTU_getValue(store, CSTU_toArray(namePath));
+    newStore = CSTU_setValue(newStore, CSTU_toArray(namePath), value);
+  });
+  return newStore;
 }
